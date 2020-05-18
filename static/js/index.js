ccea45e74be9a8c8cd6176680ca18ba5244820e7
zhouqiang <huangxin@nj.fiberhome.com.cn>
2020-05-18 15:05:22 +0800

    直播画面添加

--- static/js/index.js
+++ static/js/index.js
@@ -18,24 +18,35 @@ axios.get('/auth' + searchParam)
     showError();
 });
 
+/**
+ * 显示错误
+ */
 function showError() {
     document.getElementById("error").style.display = "block";
     document.getElementById("room").style.display = "none";
 }
 
+/**
+ * 显示房间
+ */
 function showRoom() {
     document.getElementById("room").style.display = "flex";
     document.getElementById("error").style.display = "none";
 }
 
+/**
+ * 连接
+ */
 function connect() {
     w = new WebSocket("ws://" + window.location.host + "/ws?auth=" + encodeURIComponent(info.ws_key));
     w.onopen = function () {
         console.log("已连接");
+        flv_load();
     };
 
     w.onclose = function () {
         console.log("断开连接");
+        flv_destroy();
     };
     w.onmessage = function (message) {
         var data = JSON.parse(message.data)
@@ -43,6 +54,10 @@ function connect() {
     };
 }
 
+/**
+ * 消息处理器
+ * @param data
+ */
 function resolveMsg(data) {
     if (data.type == "st") {
         sysMsg(data.message)
@@ -55,7 +70,11 @@ function resolveMsg(data) {
     }
 }
 
-
+/**
+ * 获取默认头像
+ * @param txt
+ * @returns {string}
+ */
 function getDefaultHeadImage(txt) {
 
     var canvas = document.getElementById("canvas");
@@ -79,6 +98,10 @@ function getDefaultHeadImage(txt) {
     return canvas.toDataURL('image/jpeg');
 }
 
+/**
+ * 其他用户发言展示
+ * @param data
+ */
 function userMsg(data) {
     var div = document.createElement("div");
     div.classList.add("user-info");
@@ -107,6 +130,10 @@ function userMsg(data) {
     msgPanel.scrollTop = msgPanel.scrollHeight;
 }
 
+/**
+ * 显示自己发言内容
+ * @param data
+ */
 function selfMsg(data) {
     var div = document.createElement("div");
     div.classList.add("user-info");
@@ -136,6 +163,10 @@ function selfMsg(data) {
     msgPanel.scrollTop = msgPanel.scrollHeight;
 }
 
+/**
+ * 系统消息展示
+ * @param msg
+ */
 function sysMsg(msg) {
     var div = document.createElement("div");
     div.classList.add("sys-info");
@@ -147,6 +178,9 @@ function sysMsg(msg) {
     msgPanel.scrollTop = msgPanel.scrollHeight;
 }
 
+/**
+ * 发送消息
+ */
 function send() {
     var text = document.getElementById('sayText').value;
     if (text == "") {
@@ -155,4 +189,43 @@ function send() {
     w.send(text);
     document.getElementById('sayText').value = "";
     document.getElementById('sayText').focus();
+}
+
+var flvPlayer;
+
+/**
+ * 打开直播画面
+ * @param hasAudio
+ */
+function flv_load(hasAudio=true){
+    if (flvjs.isSupported()) {
+        flv_destroy();
+        var videoElement = document.getElementById('videoElement');
+        flvPlayer = flvjs.createPlayer({
+            type: 'flv',
+            hasAudio: hasAudio,
+            isLive:true,
+            url: info.live_url
+        });
+        flvPlayer.attachMediaElement(videoElement);
+        flvPlayer.on(flvjs.Events.METADATA_ARRIVED,function(e){
+            if((!e.audiosamplerate || e.audiosamplerate === 0) && flvPlayer._mediaDataSource.hasAudio){
+                flv_load(false);
+            }
+        })
+        flvPlayer.load(); //加载
+    }
+}
+
+/**
+ * 关闭直播画面
+ */
+function flv_destroy() {
+    if (typeof flvPlayer !== "undefined" && flvPlayer != null) {
+        flvPlayer.pause();
+        flvPlayer.unload();
+        flvPlayer.detachMediaElement();
+        flvPlayer.destroy();
+        flvPlayer = null;
+    }
 }
